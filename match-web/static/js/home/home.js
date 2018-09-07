d95969b39c20a3c1a70aa0321058f64aecc07da0
wanghao <1750110604@qq.com>
2018-09-07 21:28:47 +0800

    绑首页数据

--- match-web/static/js/home/home.js
+++ match-web/static/js/home/home.js
@@ -3,13 +3,7 @@ $(function () {
      * 倒计时
      */
     countTime("2018-09-04 23:23:23");
-    /**
-     * 大体动态
-     */
-    $('#textScroll').textScroll({
-        itemBox:'.scroll-item',
-        outBox: '#scroll-item-content'
-    })
+
     /**
      * 星球上下浮动
      */
@@ -21,6 +15,7 @@ $(function () {
      * 选择进入星球
      */
     $('.star-top img').on('click',function () {
+        $('input#isShowPopup').val(1);
         //星球重新排序
         initStarPoint();
         //去掉进度条
@@ -35,7 +30,12 @@ $(function () {
      * 鼠标悬浮显示弹框
      */
     $('.star-box').hover(function () {
-        $(this).find('.popup-tip').fadeIn();
+        var category = $(this).find('p.name').html();
+        if(category && parseInt($('input#isShowPopup').val()) != -1){
+
+            requestPopupData(category, this);
+        }
+        // $(this).find('.popup-tip').fadeIn();
     }, function () {
         $(this).find('.popup-tip').fadeOut();
     })
@@ -43,7 +43,152 @@ $(function () {
      * 广告滚动
      */
     noticeRolling();
+    /**
+     * 积分榜
+     */
+    requestScoreList();
+    /**
+     * 答题动态
+     */
+    requestDynamic();
+    window.setInterval(function () {
+        requestDynamic();
+    }, 10000) // 10s请求一次
+
 })
+
+/**
+ * 积分榜请求
+ */
+function requestScoreList() {
+    $.ajax({
+        url: 'http://s.hackcoll.com:3334/challenges/users_ranking/',
+        type: 'get',
+        data:{},
+        dataType: 'json',
+        timeout: 1000,
+        success: function (data) {
+            var arr = [];
+            $('ul#score-rank-list').html('');
+            for(var i = 0; i < 10; i++){
+                if(data[i]){
+                    if(i<3){
+                        arr.push('<li>\n' +
+                            '    <div class="rank">\n' +
+                            '        <img src="../../static/img/home/img_no'+(i+1)+'.png" alt="第一">\n' +
+                            '    </div>\n' +
+                            '    <div class="name">\n' +
+                            '        '+data[i].nickname+'<br/><i>'+data[i].points+'</i>\n' +
+                            '    </div>\n' +
+                            '</li>');
+                    }else{
+                        arr.push('<li>\n' +
+                            '    <div class="rank">\n' +
+                            '        NO.'+(i+1)+'\n' +
+                            '    </div>\n' +
+                            '    <div class="name">\n' +
+                            '        '+data[i].nickname+'<br/><i>'+data[i].points+'</i>\n' +
+                            '    </div>\n' +
+                            '</li>');
+                    }
+                }else{
+                    break;
+                }
+
+
+            }
+            $('ul#score-rank-list').append(arr.join(''));
+        },
+        fail: function (err) {
+            layer.alert(err);
+        }
+    })
+}
+
+/**
+ * 答题动态请求
+ */
+
+function requestDynamic() {
+    $.ajax({
+        url: 'http://s.hackcoll.com:3334/challenges/api/solved/?id=' + $('input#Dynamic-Id').val(),
+        type: 'get',
+        data:{},
+        dataType: 'json',
+        timeout: 1000,
+        success: function (data) {
+            if (data.code == 200){
+                $('input#Dynamic-Id').val(data.first_id);
+                $('ul#scroll-item-content').html('');
+                var item = data.message;
+                var arr = [];
+                item.forEach(function (value, index) {
+                    arr.push('<li class="scroll-item">' +
+                            '<div class="content">' +
+                            '<i>'+value.name+'</i>完成'+value.challenge_name+'题，获得' +
+                            '<i>'+value.points+'分</i>，当前累积得分' +
+                            '<i>'+value.total_points+'分</i>，团队总分' +
+                            '<i>'+value.total_points+'分</i>' +
+                            '</div>' +
+                            '</li>');
+                })
+                $('ul#scroll-item-content').append(arr.join(''));
+                /**
+                 * 大体动态
+                 */
+                $('#textScroll').textScroll({
+                    itemBox:'.scroll-item',
+                    outBox: '#scroll-item-content'
+                })
+            }
+        },
+        fail: function (err) {
+            layer.alert(err);
+        }
+    })
+}
+
+/**
+ * 弹窗数据
+ */
+function requestPopupData(category, _this){
+    $.ajax({
+        url: 'http://s.hackcoll.com:3334/challenges/category_ranking/?category='+category,
+        type: 'get',
+        data:{},
+        dataType: 'json',
+        timeout: 1000,
+        success: function (data) {
+            $('ul#popup-rank-list').html('');
+            var arr = [];
+            for(var i = 0; i < 3; i++){
+                if(data[i]){
+                    arr.push('<li>'+data[i]+'</li>');
+                }else{
+                    break;
+                }
+
+            }
+            $('ul#popup-rank-list').append(arr.join(''));
+            if(data[0]){
+                $(_this).find('.popup-tip').fadeIn();
+            }
+        },
+        fail: function (err) {
+            layer.alert(err);
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
 
 
 /**
