b85bde378069792fb617ed255ef10d4efa426a91
winner <1750110604@qq.com>
2018-09-12 17:17:02 +0800

    倒计时

--- match-web/static/js/share/countTime.js
+++ match-web/static/js/share/countTime.js
@@ -2,6 +2,7 @@
  * 倒计时
  * @param time 截至时间
  * 格式："2016-10-22 23:23:23"
+ * 注意：time不能小于目前的时间，当time小于或等于现在的时间时，则倒计时置0
  */
 function countTime(time) {
     //获取当前时间
@@ -11,18 +12,26 @@ function countTime(time) {
     var endDate = new Date(time);
     var end = endDate.getTime();
     //时间差
-    var leftTime = Math.abs(end-now);
+    if(end <= now){
+        document.getElementById("counttime").innerHTML = '00:00:00';
+        return;
+    }
+    var leftTime = end-now;
     //定义变量 d,h,m,s保存倒计时的时间
     var d,h,m,s;
     if (leftTime>=0) {
-        d = Math.floor(leftTime/1000/60/60/24);
         h = Math.floor(leftTime/1000/60/60%24);
         m = Math.floor(leftTime/1000/60%60);
         s = Math.floor(leftTime/1000%60);
     }
+    if(h == 0 && m == 0 && s == 0){
+        document.getElementById("counttime").innerHTML = '00:00:00';
+        return;
+    }
     h = h<10? '0'+h:h;
     m = m<10? '0'+m:m;
     s = s<10? '0'+s:s;
+
     //将倒计时赋值到div中
     document.getElementById("counttime").innerHTML = h+':'+m+':'+s;
     //递归每秒调用countTime方法，显示动态时间效果
